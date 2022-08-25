cc3fd5a8df1fe90d227b697fec55ae57eec9e7e0
Tisbus <yog78@mail.ru>
2022-08-25 11:23:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/downloadwebcontent/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.downloadwebcontent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.ExecutionException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String mailRu = "https://mail.ru";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("Hello", mailRu);
+
+        DownloadTask task = new DownloadTask();
+        try {
+            String result = task.execute(mailRu).get();
+            Log.i("URL", result);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class DownloadTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.i("URL", strings[0]);
+            return "Готово";
+        }
+    }
+
+
+}
